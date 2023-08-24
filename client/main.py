cfbf681899cd5d0eda36364ac363344c1de78e0d
pedropcventura <pedropventura@icloud.com>
2023-08-24 15:02:04 -0300

    gerando lista de comandos

--- client/main.py
+++ client/main.py
@@ -1,3 +1,6 @@
+import random
+
+
 commands = {
     1: [0x00, 0x00, 0x00, 0x00],
     2: [0x00, 0x00, 0xBB, 0x00],
@@ -9,6 +12,15 @@ commands = {
     8: [0x00],
     9: [0xBB],
 }
+
+def generate():
+    ### Escolhe n comandos e devolve uma lista contendo todos eles
+    lstCommands = []
+    n = random.randint(10,30)
+    for _ in range(n):
+        lstCommands.append(random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+    return lstCommands
+
 
 def generate_protocol_message(command_key: int):
     m = commands[command_key]
