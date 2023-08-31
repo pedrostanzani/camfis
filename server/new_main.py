0a15ecc5e03daf7821e2033a7a9ff1d05873ad40
pedropcventura <pedropventura@icloud.com>
2023-08-31 14:29:32 -0300

    finalizado:

--- server/new_main.py
+++ server/new_main.py
@@ -13,6 +13,26 @@ c = {
     8: [0x00],
     9: [0xBB]
 }
+
+protocol_translator = {
+    1: (4, 4),
+    2: (4, 3),
+    3: (4, 2),
+    4: (4, 1),
+    5: (3, 4),
+    6: (3, 3),
+    7: (3, 2),
+    8: (3, 1),
+    9: (2, 4),
+    10: (2, 3),
+    11: (2, 2),
+    12: (2, 1),
+    13: (1, 4),
+    14: (1, 3),
+    15: (1, 2),
+    16: (1, 1),
+}
+
 def commandTranslator(command):
     for n in c.keys():
         if bytearray(c[n]) == command:
@@ -26,6 +46,9 @@ rxBuffer, nRx = com1.getData(1)
 com1.rx.clearBuffer()
 sleep(.1)
 
+def split(byteInicial):
+    return protocol_translator[byteInicial]
+
 #rxBuffer, nRx = com1.getData(32)
 commandList = []
 while True:
@@ -34,7 +57,11 @@ while True:
     if control_integer > 16:
         break
 
-    rxBuffer, nRx = com1.getData(control_integer)
+    rxBuffer, nRx = com1.getData(split(control_integer)[0])
+    commandList.append(commandTranslator(rxBuffer))
+    rxBuffer, nRx = com1.getData(split(control_integer)[1])
+    if int.from_bytes(rxBuffer, byteorder='big') == 255:
+        break
     commandList.append(commandTranslator(rxBuffer))
     print(rxBuffer)
 
@@ -46,7 +73,6 @@ print("recebeu {} bytes" .format(len(rxBuffer)))
 #     print("recebeu {}" .format(rxBuffer[i]))
 
 
-
 com1.sendData(np.asarray(bytearray([1, len(commandList), 255])))  
 
 print(str(len(commandList)) + " é a quantidade de comandos recebidos")
@@ -56,3 +82,8 @@ print("Comunicação encerrada")
 print("-------------------------")
 print()
 exit()
+
+#   Geralmente os protocolos tem um head no começo e um EOP (end of program) no final, apenas
+#   Não há bytes jogados no meio do programa. Nesse exemplo acima, o protocolo que nos criamos envia um byte
+# a cada dois comandos. Isso deveria ser feito tudo no começo, utilizando o byte de forma mais completa, talvez sub-
+# dividindo os bytes e evitando envio de bytes de protocolo soltos no meio da mensagem.
