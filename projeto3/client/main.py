60f6754ec5ecb8f286f259b2f3e577f0fdf18865
Pedro Stanzani <pedrohstanzani@gmail.com>
2023-09-14 14:04:26 -0300

    move project3NE files to project3 directory

--- projeto3/client/main.py
+++ projeto3/client/main.py
@@ -1,118 +1,140 @@
-from core.enlace import *
 import time
 import numpy as np
+from core.enlace import enlace
 
 # --------------------------------------------------------------------------------------------------
 # byte para int: int. from_bytes() -------- z = b'\x00\x00\x00\x00\x00\x01'; int.from_bytes(z,'big')
 # int para bytestring: int. to_bytes() ---- h = 1; h = h.to_bytes(6,'big')
+#   python -m serial.tools.list_ports
 # --------------------------------------------------------------------------------------------------
 
-# Entrada superior esquerda
-#serialName = '/dev/cu.usbmodem101'
-
-# Entrada direita
 serialName = '/dev/cu.usbmodem21101'
-com1 = enlace(serialName)
-com1.enable()
-
-# Martyr byte (Byte de sacrificio)
-time.sleep(0.2)
-com1.sendData(b'00')
-time.sleep(1)
 
 
-def payload_(txBuffer):
-    # payload de 50 bytes
-    lenpayload = 50
-    if len(txBuffer)<lenpayload:
-        payload = txBuffer[0:len(txBuffer)]
-        txBuffer = txBuffer[0:0]
-        return [txBuffer, payload]
-    payload = txBuffer[0:lenpayload]
-    txBuffer = txBuffer[lenpayload:]
-    return [txBuffer, payload]
+def fetch_payload(data_buffer):
+    LENGTH = 50
 
-def handshake(message = b'\xff'):
-    com1.sendData(np.asarray(message))
+    if len(data_buffer) < LENGTH:
+        payload = data_buffer[:]
+        data_buffer = []
+    else:
+        payload = data_buffer[0:LENGTH]
+        data_buffer = data_buffer[LENGTH:]
+
+    return data_buffer, payload
+
+
+def build_datagram_message(packet_id: int, total_number_of_packets: int, payload: bytes):
+    """
+    [Warning]: this function payload-size-agnostic.
+    It will build a message with no restraints when it comes to the payload size.
+    Make sure to send a payload that respects the maximum size established by the protocol.
+
+    Example heading: \x00\x00\x00\x03 \x00\x00\x00\x07 \x00\x00\x00\x50
+    Packet ID: 3
+    Total number of packets: 7
+    Payload size (in bytes): 50
+    """
+    def force_wrong_payload_size(pl, force=True):
+        return 48 if force else pl
+
+    EOP = b'\xff\xff\xff'
+    payload_size = len(payload)
+    # head = packet_id.to_bytes(4, 'big') + total_number_of_packets.to_bytes(4, 'big') + force_wrong_payload_size(payload_size).to_bytes(4, 'big')
+    head = packet_id.to_bytes(4, 'big') + total_number_of_packets.to_bytes(4, 'big') + payload_size.to_bytes(4, 'big')
+    return head + payload + EOP
+
+
+if __name__ == '__main__':
+    com1 = enlace(serialName)
+    com1.enable()
+
+    # Send a martyr byte
+    time.sleep(0.2)
+    com1.sendData(b'00')
+    time.sleep(1)
+
+    # Send handshake to server
+    com1.sendData(np.asarray(b'\xff'))
+    # Client expects server to send out feedback
     timeout_limit = time.time() + 5
     while com1.rx.getBufferLen() == 0:
         if time.time() > timeout_limit:
-            response = input("Servidor inativo. Tentar novamente? S/N: ")
-            if response == "s" or response == "S":
-                return handshake()
-            else:
+            print("[ERROR] The server is not active.")
+            response = input("Would you like to try again? [Y/N]: ")
+            if response.strip().lower() not in ['s', 'y']:
+                print("Client no longer wishes to continue. Exiting program...")
                 exit()
-    _handshake = com1.getData(1)[0]
-    if _handshake == b'\xff':
-        return True
-    else:
-        return False
+            else:
+                # Send handshake to server
+                com1.sendData(np.asarray(b'\xff'))
+                # Client expects server to send out feedback
+                timeout_limit = time.time() + 5
 
-            
-def confirmacao(NumeroDoPacote):
-
-    if com1.rx.getBufferLen() == 0:
-        return False
-
-    if com1.rx.getBufferLen() > 5:
-        IR = com1.getData(com1.rx.getBufferLen())[0]
-        
-        ndp = IR[0:6]
-        print()
-        print("INFO RECEBIDA:", IR)
-        print("TAMANHO DA INFO:", len(IR))
-        print("INFO IMPORTANTE:", ndp)
-        print("INFO ESPERADA:", NumeroDoPacote)
-        
-
-        com1.rx.clearBuffer()
-        if int.from_bytes(ndp, 'big') == NumeroDoPacote:
-            print("PASSOU NO PACOTE " + str(NumeroDoPacote))
-            return True
-        else:
-            return False
-    
-
-def main():
-    with open('/Users/pedroventura/Semestres/4/Camada_Insper/Projeto2/camfis-projeto-2/projeto3/agro.png', 'rb') as image:
-        BytesImage = image.read()
-
-    if not handshake():
-        print("HANDSHAKE RECEBIDO DIFERENTE DO ESPERADO")
+    rxBuffer, nRx = com1.getData(1)
+    if rxBuffer != b'\xff':
+        print("[ERROR] Wrong handshake.")
         exit()
 
+    print("Handshake approved.")
 
-    NumeroDoPacote = 1
-    txBuffer = BytesImage
-    # enviando as mensagens: 
-    while len(txBuffer) > 0:
-       
-        #   O HEAD terá os primeiros 6 bytes (mais significativos) sendo o numero do pacote
-        # e os ultimos 6 bytes (menos significativos) sendo o numero de pacotes totais (será sempre o mesmo valor)
+    #data = ([22] * 50) + ([23] * 50) + ([24] * 35)
+    with open('/Users/pedroventura/Semestres/4/Camada_Insper/Projeto2/camfis-projeto-2/projeto3/agro.png', 'rb') as image:
+        data = image.read()
+    total_number_of_packets = len(data) // 50
+    total_number_of_packets += 1 if len(data) % 50 != 0 else 0
+
+    i = 0
+    while True:
+        data, payload = fetch_payload(data)
+        if len(payload) == 0:
+            # This means that there is no more data left to send out.
+            break
+
+        def force_wrong_package_number(pn):
+            return 4 if pn == 2 else pn
+
+        # message = build_datagram_message(force_wrong_package_number(i + 1), total_number_of_packets, bytes(payload))
+        message = build_datagram_message(i + 1, total_number_of_packets, bytes(payload))
+        com1.rx.clearBuffer()
+        com1.sendData(message)
+
+        print(f"[{i + 1}] Just sent to client ({len(message)} bytes): ", message)
+        print()
+
+        # Expect confirmation and resend message if no confirmation is received
+        confirmed = False
+        while not confirmed:
+            timeout_limit = time.time() + 2
+            confirmed = True
+            while com1.rx.getBufferLen() == 0:
+                if time.time() > timeout_limit:
+                    com1.sendData(message)
+                    print(f"[{i + 1}] Just sent to client ({len(message)} bytes): ", message)
+                    print()
+                    confirmed = False
+                    break
         
-        #   O EOM (End Of Message) será FF FF FF fixo
-        EOM = b'\xff\xff\xff'
+        print("Confirmation confirmed.")
 
-        # funçao para cortar o txBuffer até o ponto que pegamos o payload
-        txBuffer, payload = payload_(txBuffer)
+        # Send package every 2 seconds when no confirmation in received
+        # timeout_limit = time.time() + 1
+        # while com1.rx.getBufferLen() == 0:
+        #     if time.time() > timeout_limit:
+        #         time.sleep(1)
+        #         com1.sendData(message)
+        #         print(f"[{i + 1}] Just sent to client ({len(message)} bytes): ", message)
+        #         print()
 
-        HEAD_MaisSignificativos = NumeroDoPacote
-        HEAD_MenosSignificativos = len(payload)
-        HEAD_MaisSignificativos = HEAD_MaisSignificativos.to_bytes(6,'big')
-        HEAD_MenosSignificativos = HEAD_MenosSignificativos.to_bytes(6,'big')
-        HEAD = HEAD_MaisSignificativos + HEAD_MenosSignificativos
-
-        DATAGRAMA = HEAD+payload+EOM
-        
-        #com1.rx.clearBuffer()
-        com1.sendData(DATAGRAMA)
-
-        while not confirmacao(NumeroDoPacote):
+        rxBuffer, nRx = com1.getData(18)
+        packet_id_received = rxBuffer[0:6]
+        payload = rxBuffer[12:15]
+        if int.from_bytes(payload, 'big') != 0:
+            print(f"[ERROR] The server has informed that there has been a transmission error with packet of id = {packet_id_received}.")
+            exit()
             pass
+        else:
+            if int.from_bytes(packet_id_received, 'big') == total_number_of_packets:
+                print("Success.")
 
-        NumeroDoPacote+=1
-
-    return "finalizou"
-
-
-main()
+        i += 1
