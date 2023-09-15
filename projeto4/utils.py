1e7171806ac61768c9a3ec383c3f74e3b235b59a
pedropcventura <pedropventura@icloud.com>
2023-09-15 14:32:47 -0300

    utils

---
+++ projeto4/utils.py
@@ -0,0 +1,18 @@
+from protocol.packet import Packet
+def split_and_assemble(imagePath, payloadSize=114):
+    payloadList = []
+    with open(imagePath, 'rb') as image:
+        data = image.read()
+    while len(data)>0:
+        if len(data)>payloadSize:
+            payload = data[0:payloadSize]
+            data = data[payloadSize:]
+        else:
+            payload = data
+            data = []
+        payloadList.append(payload)
+
+    [Packet(n, 3, len(payloadList), i+1, i) for i, n in enumerate(payloadList)]
+    
+    
+    
