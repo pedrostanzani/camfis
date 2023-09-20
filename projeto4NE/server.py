7e415756b2cedf6f2543995e6caede02406945ab
pedropcventura <pedropventura@icloud.com>
2023-09-20 13:09:58 -0300

    diagrama do server feito, falta testar

--- projeto4NE/server.py
+++ projeto4NE/server.py
@@ -28,6 +28,10 @@ class Server:
         else: 
             return False
     
+    def printConfig(self, config):
+        for k, v in config.items():
+            print(k, v)
+    
     def main(self):
         serialName = '/dev/cu.usbmodem21101'
         com1 = enlace(serialName)
@@ -35,14 +39,22 @@ class Server:
 
         contador = 0
         ocioso = True
+        print("Estou ocioso!")
 
         while ocioso: 
             if ocioso and com1.rx.getBufferLen() >= 9:
+                print('\n Mensagem 1 Recebida, aguardando verificação \n')
                 # Message 1 received
                 head = com1.getData(9)
                 if integer(head[0]) == 1 and integer(head[1]) == self.serverID:
+                    print("Verificado com sucesso:")
+                    print("h0 recebido igual ao esperado (1)")
+                    print("h1 recebido igual ao esperado (17) \n")
                     ocioso = False
-                else: 
+                else:
+                    print("Erro na Verificação do handshake: ") 
+                    print("h0 recebido: " + str(integer(head[0])) + "\n h0 esperado: 1")
+                    print("h1 recebido: " + str(integer(head[1])) + "\n h0 esperado: 17")
                     time.sleep(1)
         
         # ocioso = False: 
@@ -57,19 +69,30 @@ class Server:
         message1 = Packet(b'', config)
         com1.sendData(message1)
 
+        print("-------------------------")
+        print("Mensagem 0 enviada: ")
+        self.printConfig(config)
+        print("-------------------------")
+
         contador +=1 
         message=[0,0,0,0,0,0,0,0,0,0]
         while contador <= head[3]:
             timeout_limit_1 = time.time()
             timeout_limit_2 = time.time() 
             if com1.rx.getBufferLen() >= 9:
+                print("\nMENSAGEM RECEBIDA: ")
                 message = com1.getData(com1.rx.getBufferLen())
+                print(message)
+                print()
+
                 if self.verifyType3(message[0:10], contador-1, len(message)-10-4):
+                    print("--------------------------------------------------")
+                    print('PACKET RECEBIDO CONFORME ESPERADO!')
                     # packet ok
                     config = {
                     'messageType': 4,
                     'serverID': 17,
-                    'totalNumberOfPackets': integer(message[4]),
+                    'totalNumberOfPackets': integer(message[3]),
                     'packetID': contador,
                     'fileID': 0,
                     'lastSuccessfulPacket': contador-1,
@@ -77,16 +100,52 @@ class Server:
                     p = Packet(b'', config)
                     contador+=1
                 else: 
+                    print("--------------------------------------------------")
+                    print('PACKET RECEBIDO DIFERNTE DO ESPERADO!')
                     #packet not ok
                     config = {
                     'messageType': 6,
                     'serverID': 17,
-                    'totalNumberOfPackets': integer(message[4]),
+                    'totalNumberOfPackets': integer(message[3]),
                     'packetID': contador,
                     'fileID': 0,
                     'lastSuccessfulPacket': contador-1,
+                    'resendPacket': integer(message[4])
                     }
                     p = Packet(b'', config)
+                com1.sendData(p)
+                print("MENSAGEM ENVIADA: ")
+                self.printConfig(config)
+                print("--------------------------------------------------")
+                
+            else: 
+                print("MENSAGEM NÃO RECEBIDA\n")
+                time.sleep(1)
+                if time.time() - timeout_limit_2 > 20: 
+                    ocioso = True
+                    print("------------ TIMER 2 PASSOU DE 20 SEGUNDOS ------------")
+                    config = {
+                    'messageType': 5,
+                    'serverID': 17,
+                    'totalNumberOfPackets': integer(message[3]),
+                    'packetID': contador,
+                    'fileID': 0,
+                    'lastSuccessfulPacket': contador-1,
+                    'resendPacket': integer(message[4])
+                    }
+                    p = Packet(b'', config)
+                    com1.sendData(p)
+
+                    print("TEMPO ESGOTADO, mensagem tipo 5 enviada")
+                    self.printConfig(config)
+                    exit()
+
+                elif time.time() - timeout_limit_1 > 2:
+                    print("ZERANDO TIMER 1 SEI LA PRA QUE!!!!!!!!!!!")
+                    print("Nao vou mandar mensagem porra nenhuma porque nao faz sentido enviar tipo 4")
+                    timeout_limit_1 = time.time()
+
+                    
 
 
 
